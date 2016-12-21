d2da8915614bd96952cf010e7639d83a63e15294
Kirill Myagkov <kirillmyagkov17@gmail.com>
2016-12-21 17:33:04 +0300

    Главная+Фотогалерея+Конкурс. Оформление контента и декоративных элементов ч.6

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 var open = document.querySelector(".main-menu__toggle");
 var popup = document.querySelector(".main-menu__list");
-var logo = document.querySelector(".logo--page-header");
+var menu = document.querySelector(".main-menu");
 
 open.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.toggle("main-menu__list--open");
-  logo.classList.toggle("logo--no-transparent");
   open.classList.toggle("main-menu__toggle--close");
+  menu.classList.toggle("main-menu--no-transparent");
 });
 
 // close.addEventListener("click", function(event) {
