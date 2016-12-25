a33c75d9c8633d06476ae62e20035e6b9161c2c4
Kirill Myagkov <kirillmyagkov17@gmail.com>
2016-12-25 20:27:54 +0300

    Финальная проверка

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var open = document.querySelector(".main-menu__toggle");
 var popup = document.querySelector(".main-menu__list");
 var menu = document.querySelector(".main-menu");
+var burger = document.querySelector(".main-menu__burger-wrapper");
 
 open.addEventListener("click", function(event) {
   event.preventDefault();
@@ -8,5 +9,9 @@ open.addEventListener("click", function(event) {
   open.classList.toggle("main-menu__toggle--close");
   menu.classList.toggle("main-menu--no-transparent");
 });
+
+menu.classList.remove("main-menu--no-js");
+popup.classList.remove("main-menu__list--no-js");
+burger.classList.remove("main-menu__burger-wrapper--no-js");
 
 svg4everybody();
